6552d3211993245e83e54f67f99713bfabb1115b
Jaylen Sandoval <jaylensandoval@Jaylens-Air>
2023-07-31 12:20:00 -0700

    First Commit

---
+++ contactList/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./Components/SelectedContact"
+import "./App.css";
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId}/>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId}/>
+      )}
+    </>
+  );
+}
